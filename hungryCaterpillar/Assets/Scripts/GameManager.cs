401b7a7e2ae51ba6176e9e46c87b0752fb88f925
Ethan Bruins <bruins.ethan@gmail.com>
2016-04-17 14:38:22 +0100

    A* pathfinding

--- hungryCaterpillar/Assets/Scripts/GameManager.cs
+++ hungryCaterpillar/Assets/Scripts/GameManager.cs
@@ -16,7 +16,6 @@ public class GameManager : MonoBehaviour {
     public Transform borderRight;
 
 	void Start () {
-
         InvokeRepeating("Spawn", 3f, 2f);
         Score = 0;
 	}
@@ -38,8 +37,8 @@ public class GameManager : MonoBehaviour {
 
     void Spawn()
     {
-        int x = (int)Random.Range(borderLeft.position.x + 1, borderRight.position.x - 1);
-        int y = (int)Random.Range(borderBottom.position.y + 1, borderTop.position.y - 1);
+        int x = (int)Random.Range(borderLeft.position.x + 2f, borderRight.position.x - 2f);
+        int y = (int)Random.Range(borderBottom.position.y + 2f, borderTop.position.y - 2f);
 
         Instantiate(food, new Vector2(x, y), Quaternion.identity);
     }
--- hungryCaterpillar/Assets/Scripts/enemyBehaviour.cs
+++ hungryCaterpillar/Assets/Scripts/enemyBehaviour.cs
@@ -1,41 +1,166 @@
 ﻿using UnityEngine;
+using Pathfinding;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
 public class enemyBehaviour : MonoBehaviour {
 
-    private Transform[] food;
-    private NavMeshAgent agent;
-    private Vector3 goal;
-	// Use this for initialization
-	void Start () {
-        agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        GameObject[] food = GameObject.FindGameObjectsWithTag("Food");
-        goal = new Vector3(GetClosestTarget(food).position.x, GetClosestTarget(food).position.y, GetClosestTarget(food).position.z);
-        agent.destination = goal;
-	}
+    public Transform target;
 
-    Transform GetClosestTarget(GameObject[] food)
+    //How often we update the path
+    public float updateRate = 2f;
+
+    //Caching
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //The path
+    public Path path;
+
+    //Ai speed/s
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    private bool ate = false; 
+
+    private bool searchingForTarget = false;
+
+    //The max distance from AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+
+    private int currentWaypoint = 0;
+
+    public GameObject tailprefab;
+    List<Transform> tail = new List<Transform>();
+
+    void Start()
     {
-        Transform bestTarget = null;
-
-        float closestDistanceSqr = Mathf.Infinity;
-        Vector3 currentPosition = transform.position;
-        foreach (GameObject potentialTarget in food)
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        if (target == null)
         {
-            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
-            float dSqrToTarget = directionToTarget.sqrMagnitude;
-            if (dSqrToTarget < closestDistanceSqr)
+            if (!searchingForTarget)
             {
-                closestDistanceSqr = dSqrToTarget;
-                bestTarget = potentialTarget.transform;
+                searchingForTarget = true;
+                StartCoroutine(SearchForTarget());
             }
+            return;
+        }
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator SearchForTarget()
+    {
+        GameObject sResult = GameObject.FindGameObjectWithTag("Food");
+        if (sResult == null)
+        {
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(SearchForTarget());
+        }
+        else
+        {
+            target = sResult.transform;
+            searchingForTarget = false;
+            StartCoroutine(UpdatePath());
         }
 
-        return bestTarget;
+    }
+
+
+    IEnumerator UpdatePath()
+    {
+        if (target == null)
+        {
+            if (!searchingForTarget)
+            {
+                searchingForTarget = true;
+                StartCoroutine(SearchForTarget());
+            }
+        }
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+        yield return new WaitForSeconds(1f/updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Path " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            if (!searchingForTarget)
+            {
+                searchingForTarget = true;
+                StartCoroutine(SearchForTarget());
+            }
+            return;
+        }
+
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+            {
+                return;
+            }
+
+            pathIsEnded = true;
+            return;
+        }
+
+        pathIsEnded = false;
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+        Vector2 v = transform.position;
+        rb.AddForce(dir, fMode);
+        if (ate)
+        {
+            GameObject g = (GameObject)Instantiate(tailprefab, v, /*Rotation.currAngle*/ Quaternion.identity);
+            tail.Insert(0, g.transform);
+            ate = false;
+        }
+        else if (tail.Count > 0)
+        {
+            tail.Last().position = v;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count() - 1);
+        }
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Food")
+        {
+            ate = true;
+            Destroy(col.gameObject);
+        }
     }
 }
